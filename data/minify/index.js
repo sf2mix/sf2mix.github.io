efa57c415d4bdc868a80e79dff2b5d3eefcf75b8
sfz3mix <davizito.felipe@gmail.com>
2025-03-23 13:30:18 -0300

    .

--- data/minify/index.js
+++ data/minify/index.js
@@ -2,22 +2,26 @@ const UglifyJS = require("uglify-js");
 const fs = require('fs');
 const uglifycss = require('uglifycss');
 
-if (!String.prototype.replaceAll) {
-    String.prototype.replaceAll = function(a, b) {
-        return this.split(a).join(b);
-    }
+let files = [
+    'nipplejs.js',
+    'shaders.js',
+    'storage.js',
+    'gamepad.js',
+    'GameManager.js',
+    'socket.io.min.js',
+    'emulator.js'
+]
+let code = "(function() {\n";
+for (let i=0; i<files.length; i++) {
+    code += fs.readFileSync('../'+files[i], 'utf8') + "\n";
 }
-
-var a = fs.readFileSync('../emu-main.js', 'utf8').substring(18);
-var code = fs.readFileSync('../emulator.js', 'utf8').replaceAll('window.EJS_main', a);
+code += "\n})();"
 
 function minify(source){
-    var ast = UglifyJS.parse(source);
+    const ast = UglifyJS.parse(source);
     return UglifyJS.minify(ast).code;
 }
 console.log('minifying');
-fs.writeFileSync('../emu-css.min.css', uglifycss.processString(fs.readFileSync('../emu-css.css', 'utf8')));
-var min = minify(code);
+fs.writeFileSync('../emulator.min.css', uglifycss.processString(fs.readFileSync('../emulator.css', 'utf8')));
+fs.writeFileSync('../emulator.min.js', minify(code));
 console.log('done!');
-
-fs.writeFileSync('../emulator.min.js', min);
